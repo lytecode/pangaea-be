2834298c9749724c8feb685c5c1420fcef9f4552
lytecode <mbonubasil@gmail.com>
2021-02-03 21:10:06 +0100

    Setup publisher server

--- publisher/server.js
+++ publisher/server.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const redis = require('redis');
+const Joi = require('joi')
 
+const app = express();
 const publisher = redis.createClient();
 
-app.get('/', (req, res) => {
-    const user = {
-        id: '12345',
-        name: 'Testkit'
-    }
+const PORT = 8000;
+app.use(express.json())
+
+const schema = Joi.object({
+    topic: Joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+
+    data: Joi.object({
+        msg: Joi.string().min(3).required()
+    })
 })
+
+app.post('/publish/:topic', (req, res) => {
+    const { topic } = req.params;
+    const { body } = req;
+
+    const { error } = schema.validate({ topic, data: body });
+    if (error) {
+        return res.status(400).json({ msg: error.details[0].message })
+    }
+
+    publisher.publish(topic, JSON.stringify(body));
+    res.status(201).json({ msg: `Message publised to '${topic}' topic` })
+})
+
+// Catch all non existent routes
+app.use((req, res) => {
+    res.status(404).json({ msg: 'Not found' });
+});
+
+app.listen(PORT, () => console.log(`server publisher started on port ${PORT}`))
